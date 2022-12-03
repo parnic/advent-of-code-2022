771a21b969e3d859ca1fdfcf8e624cf070b06541
Parnic <github@parnic.com>
2022-12-03 08:57:51 -0600

    Day 3 solution
    Took me a bit to remember how Aggregate worked, as it always does when I need it.

---
+++ src/03.cs
@@ -0,0 +1,37 @@
+﻿namespace aoc2022;
+
+internal class Day03 : Day
+{
+    private IEnumerable<string>? sacks;
+
+    internal override void Parse()
+    {
+        sacks = Util.ReadAllLines("03");
+    }
+
+    static int GetPriority(char x) => x <= 'Z' ? x - 'A' + 27 : x - 'a' + 1;
+
+    internal override string Part1()
+    {
+        var compartments = sacks!.Select(x => (x[..(x.Length/2)], x[(x.Length/2)..]));
+        var intersected = compartments.Select(x => x.Item1.Intersect(x.Item2).First());
+        var total = intersected.Select(GetPriority).Sum();
+
+        return $"Sum of duplicates' priorities: <+white>{total}";
+    }
+
+    internal override string Part2()
+    {
+        var groups = sacks!.Chunk(3);
+        var sum = groups.Sum(x =>
+            GetPriority(
+                x.Skip(1).Aggregate(
+                    x.First().AsEnumerable(),
+                    (l, e) => l.Intersect(e)
+                ).First()
+            )
+        );
+
+        return $"Sum of badges' priorities: <+white>{sum}";
+    }
+}
