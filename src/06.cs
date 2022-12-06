a3b906f94d06e616e134b1f97c1d8ad23d8c49de
Parnic <github@parnic.com>
2022-12-05 23:28:34 -0600

    Day 6 solution

---
+++ src/06.cs
@@ -0,0 +1,44 @@
+﻿namespace aoc2022;
+
+internal class Day06 : Day
+{
+    private string? buffer;
+
+    internal override void Parse()
+    {
+        buffer = Util.ReadAllText("06");
+    }
+
+    private static int FindDistinct(string buf, int distinctLen)
+    {
+        Queue<char> s = new(distinctLen);
+        for (int i = 0; i < buf.Length; i++)
+        {
+            var c = buf[i];
+            s.Enqueue(c);
+            if (s.Count < distinctLen)
+            {
+                continue;
+            }
+
+            if (s.Distinct().Count() == distinctLen)
+            {
+                return i + 1;
+            }
+
+            s.Dequeue();
+        }
+
+        throw new Exception("didn't find anything");
+    }
+
+    internal override string Part1()
+    {
+        return $"First span of 4 distinct characters at character <+white>{FindDistinct(buffer!, 4)}";
+    }
+
+    internal override string Part2()
+    {
+        return $"First span of 14 distinct characters at character <+white>{FindDistinct(buffer!, 14)}";
+    }
+}
